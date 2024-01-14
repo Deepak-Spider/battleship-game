693b8030b74e8ace95412219224b31381ddb9700
deepakdev <deepakp.tvla@gmail.com>
2024-01-14 19:10:25 +0000

    Implement ship sunk check

--- run.py
+++ run.py
@@ -182,3 +182,17 @@ def shoot_bullet():
 
     bullets_left -= 1
 
+# Determine if a ship is completely sunk after a bullet hit
+def check_for_ship_sunk(row, col):
+    global ship_positions
+    global grid
+
+    for position in ship_positions:
+        start_row = position[0]
+        end_row = position[1]
+        start_col = position[2]
+        end_col = position[3]
+        if start_row <= row <= end_row and start_col <= col <= end_col:
+            # Ship found, now check if its all sunk
+            for r in range(start_row, end_row):
+                for c in range(start_col, end_col
